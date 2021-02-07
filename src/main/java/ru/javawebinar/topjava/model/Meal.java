52b5ecd9cde9f3187453bec33a7b3d1c7d67e3e8
banigoden <banigoden@gmail.com>
2021-02-07 23:57:22 +0300

    Реализация через цикл.

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -4,14 +4,14 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
-public class UserMeal {
+public class Meal {
     private final LocalDateTime dateTime;
 
     private final String description;
 
     private final int calories;
 
-    public UserMeal(LocalDateTime dateTime, String description, int calories) {
+    public Meal(LocalDateTime dateTime, String description, int calories) {
         this.dateTime = dateTime;
         this.description = description;
         this.calories = calories;
@@ -28,8 +28,12 @@ public class UserMeal {
     public int getCalories() {
         return calories;
     }
-    public LocalDate getDate() { return dateTime.toLocalDate();}
 
-    public LocalTime getTime() { return dateTime.toLocalTime();
+    public LocalDate getDate() {
+        return dateTime.toLocalDate();
+    }
+
+    public LocalTime getTime() {
+        return dateTime.toLocalTime();
     }
 }
